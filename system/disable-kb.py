2dfa9825ea8a40540dbd450d27a31eda1845f5e7
Dylan Bretz Jr. <dylanbretzjr@gmail.com>
2026-05-13 09:53:13 -0400

    Add support for user-specified sleep time argument

--- system/disable-kb.py
+++ system/disable-kb.py
@@ -7,19 +7,31 @@ Requirements:
 - pynput
 """
 
+import sys
 import time
 from pynput import keyboard
 
-SLEEP_TIME = 10
-
 def main():
-    print(f'Keyboard disabled for {SLEEP_TIME} seconds...')
+    sleep_time = 10
+
+    if len(sys.argv) > 1:
+        try:
+            sleep_time = int(sys.argv[1])
+            if sleep_time <= 0:
+                print("Error: Time must be greater than 0.")
+                sys.exit(1)
+        except ValueError:
+            print("Error: Please provide a valid whole number for seconds.")
+            print(f"Usage: {sys.argv[0]} [seconds]")
+            sys.exit(1)
+
+    print(f'Keyboard disabled for {sleep_time} seconds...')
 
     listener = keyboard.Listener(suppress=True)
     listener.start()
 
     try:
-        for i in range(SLEEP_TIME, 0, -1):
+        for i in range(sleep_time, 0, -1):
             print(f'Seconds remaining: {i}')
             time.sleep(1)
 
